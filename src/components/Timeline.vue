23548e33cffbbddd0e7c8cd31dbbcadb7a07469a
kikkipedia <kristin.akerlund@lir.gu.se>
2025-01-13 13:20:46 +0100

    Building mockup and deleting unnecessary files

---
+++ src/components/Timeline.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-container>
+
+    </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import timeline from '@/assets/timeline.json'
+
+const timelineData = ref(timeline.data)
+
+
+</script>
+
+<style scoped>
+</style>
