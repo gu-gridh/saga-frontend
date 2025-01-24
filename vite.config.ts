b5a4edecf107fdcb46c616dc97bb975618c78b40
kikkipedia <kristin.akerlund@lir.gu.se>
2025-01-24 14:04:34 +0100

    Fetch from API now works

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -17,4 +17,20 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+//   server: {
+//     https: {
+//       key: fs.readFileSync(path.resolve(__dirname, 'src/assets/server.key'), 'utf-8'), // Private key path
+//       cert: fs.readFileSync(path.resolve(__dirname, 'src/assets/server.crt'), 'utf-8'), // Certificate path
+//     },
+//     port: 3000,          // Specify your preferred port
+//     host: 'localhost',   // Specify the host (default is localhost)
+//     open: true,          // Automatically open in the browser
+//     proxy: {
+//         '/api': {
+//             target: 'https://saga.dh.gu.se/', // Your backend server
+//             changeOrigin: true,
+//             secure: false, // Set to true if your backend server uses a valid SSL certificate
+//         },
+//     },
+// },
 })
